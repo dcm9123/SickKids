88192d5e14bdef759c8f15c773fd2b753a325cdb
Daniel Castaneda Mogollon, PhD <35580294+dcm9123@users.noreply.github.com>
2026-02-02 16:12:31 -0700

    Add metacyc_genome_annotator.py for MinPath input preparation
    This script prepares MinPath input files from annotated EggNOGs to retrieve metabolic pathways.

---
+++ MetaCyc/metacyc_genome_annotator.py
@@ -0,0 +1,43 @@
+# Daniel Castaneda Mogollon, PhD
+# February 2nd, 2026
+# Script to prepare MinPath input files from the annotated EggNOGs (ECs) to get the metabolic pathways
+
+#%%
+import pandas as pd
+import os
+import glob as glob
+
+#%%
+path = "/Users/danielcm/Desktop/diammatics/T1D/PICRUSt2.2/"
+os.chdir(path)
+
+df = pd.read_csv("EC_for_picrust2_renamed.tsv", sep = "\t")
+genomes = df['assembly'].to_list()
+print(genomes)
+# %%
+enzyme_df = df.filter(like='EC:', axis=1)
+enzyme_df = enzyme_df.columns.tolist()
+print(enzyme_df)
+# %%
+
+i = 1
+for row in df.itertuples():
+    print(f"Processing genome {row.assembly}")
+    f_out = open(f"EC_annotated_genomes/{row.assembly}_minpath_ecs.tsv", "w")
+    i = i +1
+    j = 1
+    for column in range(1,len(df.columns),1):
+        number = df.at[row.Index, df.columns[column]]
+        number = int(number)
+        print(number)
+        if number > 0:
+            for k in range(1,number+1,1):
+                f_out.write(f"{row.assembly}_{j}\t{df.columns[column]}\n")
+                j = j+1
+        else:
+            continue
+    f_out.close()
+        
+    
+
+# %%
