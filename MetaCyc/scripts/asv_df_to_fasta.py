dd3bfe168b8677fca96a31f2744d60d8d413eacb
Daniel <dann.dignus.discere@gmail.com>
2026-04-14 11:25:12 -0600

    Changing code

--- MetaCyc/scripts/asv_df_to_fasta.py
+++ MetaCyc/scripts/asv_df_to_fasta.py
@@ -1,3 +1,10 @@
+# Daniel Castaneda Mogollon, PhD
+# April 14th, 2026
+# Purpose: This script takes as input the filtered ASV count tables, and takes the first. 
+# column with the ASV IDs in the format of a sequence. Then it generates a fasta file with those
+# IDs as the header and the sequence.
+
+
 #%%
 import pandas as pd
 import re
@@ -5,13 +12,13 @@ from Bio import SeqIO
 import glob as glob
 
 # %%
-path = "/Users/danielcm/Desktop/Sickkids/Phyloseq2/"
+path = "/Users/danielcm/Desktop/Sickkids/Phyloseq2/Filtered_to_use/"
 os.chdir(path)
-comm = ["ns1","ns6","s2","s5"]
-for file in glob.glob("*0.txt"):
-    df = pd.read_csv(file, sep = '\t')
-    sequence = df['ASV_ID'].to_list()
-    f_out_name = file.replace(".txt",".fasta")
+comm = ["NS1","NS6","S2","S5"]
+for file in glob.glob("*prev20*SRA*.csv"):
+    df = pd.read_csv(file, sep = ',')
+    sequence = df['asv_id'].to_list()
+    f_out_name = file.replace(".csv",".fasta")
     f_out = open(f_out_name,"w")
     for seq in sequence:
         f_out.write(f">{seq}\n{seq}\n")
